f961157d8845657874f874737cee2d87fbd11beb
BoyChai <1972567225@qq.com>
2023-05-08 23:37:09 +0800

    grpc - use 1

--- test/service_test.go
+++ test/service_test.go
@@ -1,9 +1,13 @@
 package test
 
 import (
+	"context"
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	"go-gRPC/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net"
 	"testing"
 )
 
@@ -29,6 +33,34 @@ func TestUser(t *testing.T) {
 	fmt.Println(newUser)
 }
 
-func TestUser2(t *testing.T) {
+func TestProdService(t *testing.T) {
+	// 新建一个gprc服务
+	server := grpc.NewServer()
+	// 注册
+	service.RegisterProdServiceServer(server, service.ProductService)
+	// 启动服务
+	listen, err := net.Listen("tcp", ":8002")
+	if err != nil {
+		t.Fatal("启动服务出错", err)
+	}
+	err = server.Serve(listen)
+	if err != nil {
+		t.Fatal("启动服务出错", err)
+	}
+	fmt.Println("启动grpc服务成功")
+}
 
+func TestClientProdService(t *testing.T) {
+	conn, err := grpc.Dial(":8002", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatal("链接出错:", err)
+	}
+	// 退出时关闭
+	defer conn.Close()
+	client := service.NewProdServiceClient(conn)
+	stock, err := client.GetProductStock(context.Background(), &service.ProductRequest{ProdId: 233})
+	if err != nil {
+		t.Fatal("链接出错:", err)
+	}
+	fmt.Println(stock.ProdStock)
 }
